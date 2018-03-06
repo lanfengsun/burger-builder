8433c2cfeb39edbbf817ee7d94a0c290b1fd9c74
Lanfeng Sun <lanfeng.sun@yahoo.ca>
2018-03-06 16:32:27 -0500

    add build controls

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder />
-          <BuildControls />
         </Layout>
       </div>
     )
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,10 +2,19 @@ import React from 'react';
 import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl'
 
+const controls = [
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Salad', type: 'salad' },
+    { label: 'Meat', type: 'meat' }
+];
+
 const buildControls = (props) => {
     return (
-        <div>
-            <BuildControl label='cheese' />
+        <div className={classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label} />
+            ))}
         </div>
     )
 }
